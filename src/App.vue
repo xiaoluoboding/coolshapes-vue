3d4e024bd0d411149cbdc1a8e2ddf7e9bcec894f
Yuwnei Xiao <xiaoluoboding@gmail.com>
2024-05-07 10:13:16 +0800

    feat: add SEO meta

--- src/App.vue
+++ src/App.vue
@@ -34,11 +34,26 @@
         <div class="font-extrabold flex-center">
           <span class="text-6xl text-neon">Coolshapes Vue</span>
         </div>
-        <div
-          class="text-2xl font-semibold text-center flex-center text-slate-700 py-4 dark:text-slate-200"
+        <h1
+          class="text-2xl font-semibold text-center flex-center text-neutral-700 py-4 dark:text-neutral-200"
         >
-          A vue component library for coolshapes with little grainy gradients.
-        </div>
+          A Vue component library for coolshapes with little grainy gradients.
+        </h1>
+        <p class="text-lg text-center font-500 text-neutral-500">
+          <strong class="text-neutral-300">100+ Abstract shapes</strong> with
+          cool grainy gradient.
+        </p>
+        <p class="text-lg text-center font-500 text-neutral-500">
+          Ready to use with any design dev project.
+        </p>
+        <p class="text-sm text-center font-500 text-neutral-500 py-2">
+          Open-Source with MIT License, Created by
+          <a class="text-neon" href="https://x.com/realvjy">@realvjy</a>, Ported
+          by
+          <a class="text-neon" href="https://x.com/robert_shaw_x"
+            >@robert_shaw_x</a
+          >
+        </p>
         <div class="flex-center flex-col lg:flex-row gap-4 mt-8">
           <LinkButton
             href="https://github.com/xiaoluoboding/coolshapes-vue"
@@ -142,7 +157,7 @@
 
       <footer
         class="my-16 w-full flex-center text-primary"
-        text="slate-900 dark:slate-300 opacity-60 sm"
+        text="neutral-900 dark:neutral-300 opacity-60 sm"
       >
         <div class="copyright flex flex-col justify-center items-center">
           <p>
@@ -196,6 +211,8 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { useSeoMeta } from '@unhead/vue'
+
 import {
   Star,
   Flower,
@@ -210,6 +227,21 @@ import {
   Coolshape
 } from '@/index'
 import { isDark, toggleDarkmode } from '~/composables/useDarkmode'
+
+useSeoMeta({
+  ogTitle: 'Coolshapes Vue',
+  title: 'Coolshapes Vue',
+  twitterTitle: 'Coolshapes Vue',
+  description:
+    '100+ Abstract shapes with cool grainy gradient. Ready to use with any design dev project.',
+  ogDescription:
+    '100+ Abstract shapes with cool grainy gradient. Ready to use with any design dev project.',
+  twitterDescription:
+    '100+ Abstract shapes with cool grainy gradient. Ready to use with any design dev project.',
+  ogImage: 'https://coolshapes-vue.vercel.app/preview.jpg',
+  twitterImage: 'https://coolshapes-vue.vercel.app/preview.jpg',
+  ogLocale: 'en'
+})
 
 const isNoise = ref(true)
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createHead } from '@unhead/vue'
 
+import App from './App.vue'
 import 'uno.css'
 import '@unocss/reset/tailwind-compat.css'
 import highlight from '~/plugins/highlight'
 
 const app = createApp(App)
+const head = createHead()
 
+app.use(head)
 app.use(highlight)
 app.mount('#app')
